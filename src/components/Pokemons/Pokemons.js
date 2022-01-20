2cc29a2804c435959c6f336e624e25d49a5a9d0a
Matías Fuentes <matiasf.cortez@gmail.com>
2022-01-20 13:02:07 -0300

    fix context and useless Fragment, and div on Pokemons

--- src/components/Pokemons/Pokemons.js
+++ src/components/Pokemons/Pokemons.js
@@ -1,18 +1,18 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import Grid from "@material-ui/core/Grid";
 import Pokemon from "./Pokemon";
+import { PokemonContext } from "../../contexts/PokemonContext";
 
-const Pokemons = ({ pokemonss }) => (
-  <Fragment>
-    <div>
+const Pokemons = () => {
+  const { pokemonss } = useContext(PokemonContext)
+  return(
+  
       <Grid container spacing={3} justify="center">
         {pokemonss?.map((pokemon) => {
-          const { name, url } = pokemon.results;
-
+          const { name, url } = pokemon;
           return <Pokemon key={name} name={name} url={url} />;
         })}
       </Grid>
-    </div>
-  </Fragment>
-);
+)}
+
 export default Pokemons;
